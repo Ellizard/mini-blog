62de44fa90ec7c7f294a9ddbd8a9d354b75738f5
Artem Ostavnenko <artem.ostavnenko@sap.com>
2020-04-13 22:34:32 +0300

    Add admin links

---
+++ src/components/postTeaser/adminLinks/adminLinks.js
@@ -0,0 +1,20 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import classes from './adminLinks.module.scss';
+
+const adminLinks = (props) => {
+	return (
+		<div className={classes.adminAction}>
+			<IconButton aria-label="delete">
+				<DeleteIcon color="secondary" fontSize="small" />
+			</IconButton>
+			<IconButton aria-label="edit">
+				<EditIcon color="primary" fontSize="small" />
+			</IconButton>
+		</div>
+	);
+};
+
+export default adminLinks;
