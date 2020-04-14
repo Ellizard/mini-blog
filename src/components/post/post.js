3bb4ff34875ba7b08cf910040a53e0c692d7a441
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2020-04-14 13:16:39 +0300

    lodash

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -3,7 +3,7 @@ import {withRouter} from 'react-router-dom';
 import Comment from './comment/comment';
 
 const post = (props) => {
-	const ID = +props.match.params.id;
+	const ID = props.match.params.id;
 	const filteredPost = props.posts.filter(function (ele) {
 		return ele.id === ID;
 	});
